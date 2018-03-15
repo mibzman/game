51bffc5e07ef90a420b083eb24093456fb9cba53
mibzman <sam@hungerperks.com>
2018-03-15 09:22:30 -0400

    added super basics

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,17 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+	Height: number;
+	Width: number;
 
+  constructor(
+  	public navCtrl: NavController,
+  	private Plat: Platform,
+  	) {
+  	Plat.ready().then((readySource) => {
+  	      this.Width = Plat.width();
+  	      this.Height = Plat.height() * .57 ;
+  	    });
   }
 
 }
