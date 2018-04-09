a9ffdeb3f5fbf5a7d9ead8ade3f39247284d5890
mibzman <sam@hungerperks.com>
2018-04-09 11:09:00 -0400

    scores degrade, color changes

--- src/components/bar/bar.ts
+++ src/components/bar/bar.ts
@@ -1,21 +1,35 @@
-import { Component, Input } from '@angular/core';
-
+import { Component, Input } from "@angular/core";
 
 @Component({
-  selector: 'bar',
-  templateUrl: 'bar.html'
+	selector: "bar",
+	templateUrl: "bar.html"
 })
 export class BarComponent {
+	@Input("progress") progress;
 
-  @Input('progress') progress;
+	color = "#488aff";
 
-  constructor() {
-  }
+	constructor() {}
 
-  ngOnInit(){
-  	if (this.progress > 100) {
-  		this.progress = 100
-  	}
-  }
+	ngOnChanges() {
+		if (this.progress > 100) {
+			this.progress = 100;
+		}
 
+		if (this.progress > 50) {
+			this.color = "#488aff";
+		}
+
+		if (this.progress < 0) {
+			this.progress = 0;
+		}
+
+		if (this.progress < 30) {
+			this.color = "#fec907";
+		}
+
+		if (this.progress < 10) {
+			this.color = "#f53d3d";
+		}
+	}
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,81 +1,139 @@
-import { Component } from '@angular/core';
-import { NavController, Platform } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController, Platform } from "ionic-angular";
 
-import {AnimationState, AnimationController} from './Animations';
+import { AnimationState, AnimationController } from "./Animations";
 
 class Monster {
-	Idle: string[]
-	Happy: string[]
-	Eating: string[]
+  constructor() {
+    this.Idle = [
+      "assets/imgs/characters/1/neutral.png",
+      "assets/imgs/characters/1/idle1.png",
+      "assets/imgs/characters/1/neutral.png",
+      "assets/imgs/characters/1/idle2.png",
+      "assets/imgs/characters/1/neutral.png"
+    ];
+    this.Happy = [
+      "assets/imgs/characters/1/happy1.png",
+      "assets/imgs/characters/1/happy2.png",
+      "assets/imgs/characters/1/happy3.png",
+      "assets/imgs/characters/1/happy2.png",
+      "assets/imgs/characters/1/happy1.png"
+    ];
+    this.Eating = [
+      "assets/imgs/characters/1/eating1.png",
+      "assets/imgs/characters/1/eating2.png",
+      "assets/imgs/characters/1/eating3.png",
+      "assets/imgs/characters/1/eating2.png",
+      "assets/imgs/characters/1/eating3.png",
+      "assets/imgs/characters/1/eating2.png"
+    ];
+
+    this.Scores = {
+      Hungry: 50,
+      Happy: 50,
+      Clean: 50,
+      Smart: 50
+    };
+  }
+
+  Idle: string[];
+  Happy: string[];
+  Eating: string[];
+
+  Scores: {
+    Hungry: number;
+    Happy: number;
+    Clean: number;
+    Smart: number;
+  };
+
+  // LastDegrade: number = Date.now()
+
+  TryDegradeScores() {
+    // if (this.LastDegrade > new Date(Date.now() - 6000) ) {
+    //   return
+    // }
+
+    this.Scores.Hungry -= 1;
+    this.Scores.Clean -= 1;
+    this.Scores.Smart -= 1;
+
+    this.Scores.Happy =
+      (this.Scores.Hungry + this.Scores.Clean + this.Scores.Smart) / 3;
+
+      // this.LastDegrade = Date.now()
+  }
 }
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
-	
-	MonsterZoneHeight: number;
-	MonsterZoneWidth: number;
-	MonsterZoneSrc: string;
+  MonsterZoneHeight: number;
+  MonsterZoneWidth: number;
+  MonsterZoneSrc: string;
 
-	Monster: Monster = new Monster()
+  Monster: Monster = new Monster();
 
-	AnimationController: AnimationController = new AnimationController()
+  AnimationController: AnimationController = new AnimationController();
 
-  constructor(
-  	public navCtrl: NavController,
-  	private Plat: Platform,
-  	) {
-	  	Plat.ready().then((readySource) => {
-		      this.MonsterZoneWidth = Plat.width();
-		      this.MonsterZoneHeight = Plat.height() * .57 ;
-		    });
-	  	this.Monster.Idle = ["assets/imgs/characters/1/neutral.png", "assets/imgs/characters/1/idle1.png", "assets/imgs/characters/1/neutral.png", "assets/imgs/characters/1/idle2.png", "assets/imgs/characters/1/neutral.png",]
-	  	this.Monster.Happy = ["assets/imgs/characters/1/happy1.png", "assets/imgs/characters/1/happy2.png", "assets/imgs/characters/1/happy3.png", "assets/imgs/characters/1/happy2.png", "assets/imgs/characters/1/happy1.png"]
+  TickCounter: number = 0;
 
-	  	this.StartAnimating()
+  constructor(public navCtrl: NavController, private Plat: Platform) {
+    Plat.ready().then(readySource => {
+      this.MonsterZoneWidth = Plat.width();
+      this.MonsterZoneHeight = Plat.height() * 0.57;
+    });
+    this.StartAnimating();
   }
 
   StartAnimating() {
-  	if (this.AnimationController.Start()){
-  		this.Tick()
-  	}
+    if (this.AnimationController.Start()) {
+      this.Tick();
+    }
   }
 
-  Tick(){
-		if (this.AnimationController.State == AnimationState.Ready) {
-			this.AnimationController.State = AnimationState.Animating
-			
-			if (this.AnimationController.Queue.Length() == 0){
-				this.Animate(this.Monster.Idle)
-			} else {
-				this.AnimationController.Queue.Pop().call(this)
-			}
-		}
-  		
-  	setTimeout(() => {
-  		this.Tick()
-  	}, 75)
+  Tick() {
+    if (this.AnimationController.State == AnimationState.Ready) {
+      this.AnimationController.State = AnimationState.Animating;
+
+      if (this.AnimationController.Queue.Length() == 0) {
+        this.Animate(this.Monster.Idle);
+      } else {
+        this.AnimationController.Queue.Pop().call(this);
+      }
+    }
+
+    if (this.TickCounter >= 14 * 3){
+      this.Monster.TryDegradeScores();
+      this.TickCounter = 0  
+    }
+    
+    this.TickCounter++
+    setTimeout(() => {
+      this.Tick();
+    }, 75);
   }
 
-  Animate(arr: string[], idx: number = 0){
-  	if (arr.length == idx) {
-  		this.AnimationController.Done()
-  		return
-  	}
-  	this.MonsterZoneSrc = arr[idx]
-  	setTimeout(() => {
-  		this.Animate(arr, idx + 1)
-  	}, 1000 / arr.length)
+  Animate(arr: string[], idx: number = 0) {
+    if (arr.length == idx) {
+      this.AnimationController.Done();
+      return;
+    }
+    this.MonsterZoneSrc = arr[idx];
+    setTimeout(() => {
+      this.Animate(arr, idx + 1);
+    }, 1000 / arr.length);
   }
 
   HappyMonster() {
-  	this.Animate(this.Monster.Happy)
+    this.Animate(this.Monster.Happy);
   }
 
-  BeHappy() {
-  	this.AnimationController.Queue.Push(this.HappyMonster)
+  Feed() {
+    this.AnimationController.Queue.Push(() => {
+      this.Animate(this.Monster.Eating);
+    });
   }
-
 }
